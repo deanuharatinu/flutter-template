8051818f5174645879c907ec4d075fe81d7c6fb8
deanuharatinu <deanu.alt@gmail.com>
2024-10-02 11:50:20 +0700

    chore: add shell script to generate networking module

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 echo "⌛️ Installing flutter dependencies"
 flutter pub get;
+
+echo "⌛️ Generate Networking"
+pushd packages/networking
+flutter pub get; 
+echo "✅ Succeed Generate Networking"
+popd
